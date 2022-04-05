f853762aa84f44ac4be4baf3aa4d1355c0564b92
6-ON <aminhatim6@gmail.com>
2022-04-05 00:17:17 +0000

    adding mahapps package to modernise UI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,13 +12,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace Devis_Factures_Remake
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
